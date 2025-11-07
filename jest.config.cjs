8684244a83aa3d3eda990efd5c4049d69a8c90c3
Ahmed Fawzy <eng.ahmedmohamedfawzy2025@gmail.com>
2025-11-07 14:46:23 +0200

    chore: ensure jest config and workflow present

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,7 @@
-// jest.config.cjs
 module.exports = {
   testEnvironment: 'node',
   verbose: true,
   collectCoverageFrom: ['src/**/*.js'],
   coverageDirectory: 'coverage',
   transform: {}
-
 };
